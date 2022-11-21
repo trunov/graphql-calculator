e6075b5c6fa33c845b69a5a2347a3df4ccfb6cbb
trunov <k.trunov.k@yandex.ru>
2022-11-21 19:31:53 +0700

    initial typegraphql setup with express server

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import "reflect-metadata";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloWorldResolver } from "./resolvers/HelloWorldResolver";
+import { port } from "./constants/constants";
+
+export const setupServer = async () => {
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloWorldResolver],
+    }),
+    context: ({ req, res }) => ({ req, res }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app, cors: true });
+  return app;
+};
+
+export const startServer = async () => {
+  const app = await setupServer();
+
+  app.listen(port, () => {
+    console.log(`express server has started on port: ${port}`);
+  });
+};
+
+startServer();
